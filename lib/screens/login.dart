2e527f4ef1ed50036e90bf606a317e6ed2a526bc
raghav gupta <raghavguptaeng@users.noreply.github.com>
2021-05-17 01:14:17 +0530

    OTP Screen Made

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -84,7 +84,7 @@ class _LoginState extends State<Login> {
         index: currIndex,
         children: [
           GetPhoneNumber(context),
-          Container()
+          OTP(context)
         ],
       )),
     );
@@ -109,10 +109,10 @@ class _LoginState extends State<Login> {
                       ),
                       const SizedBox(width: 20.0, height: 100.0),
                       SizedBox(
-                        width: 250,
+                        width: 220,
                         child: DefaultTextStyle(
                           style: const TextStyle(
-                            fontSize: 40.0,
+                            fontSize: 35.0,
                             fontFamily: 'Horizon',
                             fontWeight: FontWeight.bold,
                             color: Colors.black
@@ -184,38 +184,97 @@ class _LoginState extends State<Login> {
           ),
         );
   }
+
+  Container OTP(BuildContext context) {
+    return Container(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height*0.2,),
+            SizedBox(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const SizedBox(width: 20.0, height: 100.0),
+                  const Text(
+                    'Stay',
+                    style: TextStyle(fontSize: 43.0),
+                  ),
+                  const SizedBox(width: 20.0, height: 100.0),
+                  SizedBox(
+                    width: 220,
+                    child: DefaultTextStyle(
+                      style: const TextStyle(
+                          fontSize: 35.0,
+                          fontFamily: 'Horizon',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black
+                      ),
+                      child: AnimatedTextKit(animatedTexts: [
+                        RotateAnimatedText('SAFE'),
+                        RotateAnimatedText('OPTIMISTIC'),
+                        RotateAnimatedText('DIFFERENT'),
+                        RotateAnimatedText('CONNECTED')
+                      ]),
+                    ),
+                  ),
+                ],
+              ),
+              height: 100,
+            ),
+            SizedBox(height: 10,),
+            Image.asset(
+              'assets/help.png',
+              width: 154,
+            ),
+            SizedBox(height: 15,),
+            Center(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: TextField(
+                  controller: _smsController,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Enter OTP',
+                    filled: true,
+                    fillColor: Colors.blue,
+                    contentPadding: const EdgeInsets.only(
+                        left: 14.0, bottom: 6.0, top: 8.0),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  keyboardType: TextInputType.phone,
+                ),
+              ),
+            ),
+            SizedBox(height: 15,),
+            GestureDetector(
+              onTap: ()async{
+                signInWithPhoneNumber();
+                Navigator.pushReplacementNamed(context, MainScreen.id);
+              },
+              child: Container(
+                width: 150,
+                height: 40,
+                child: Center(child: Text("Sign In",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),)),
+                decoration: BoxDecoration(
+                    color: Color(0xFFFDBA36),
+                    border: Border.all(color: Colors.black,width: 2),
+                    borderRadius: BorderRadius.circular(30)
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
-// Column(
-// children: [
-// TextFormField(
-//
-// decoration: InputDecoration(labelText: 'Phone number (+xx xxx-xxx-xxxx)'),
-// ),
-// Container(
-// padding: const EdgeInsets.symmetric(vertical: 16.0),
-// alignment: Alignment.center,
-// child: RaisedButton(
-// color: Colors.greenAccent[400],
-// child: Text("Verify Number"),
-// onPressed: () async {
-//
-// },
-// ),
-// ),
-// TextFormField(
-// controller: _smsController,
-// decoration: const InputDecoration(labelText: 'Verification code'),
-// ),
-// Container(
-// padding: const EdgeInsets.only(top: 16.0),
-// alignment: Alignment.center,
-// child: RaisedButton(
-// color: Colors.greenAccent[200],
-// onPressed: () async {
-// signInWithPhoneNumber();
-// Navigator.pushReplacementNamed(context, MainScreen.id);
-// },
-// child: Text("Sign in")),
-// ),
-// ],
-// ),
