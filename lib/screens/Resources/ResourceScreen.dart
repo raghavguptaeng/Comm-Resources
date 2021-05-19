b0a26945c4add967cee4dce74c9c55d7f90914cd
Raghav <e19cse258@bennett.edu.in>
2021-05-19 04:33:10 -0700

    Requirement row added on Item Screen

--- lib/screens/Resources/ResourceScreen.dart
+++ lib/screens/Resources/ResourceScreen.dart
@@ -3,7 +3,7 @@ import 'package:comm_resources/constants.dart';
 import 'package:flutter/material.dart';
 
 class ResourceInfo extends StatefulWidget {
-  ResourceInfo({this.index,this.image});
+  ResourceInfo({this.index, this.image});
   int index;
   var image;
   @override
@@ -11,22 +11,33 @@ class ResourceInfo extends StatefulWidget {
 }
 
 class _ResourceInfoState extends State<ResourceInfo> {
+  int requirement = 0;
+  String reason,contact;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
         child: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('Medicines').snapshots(),
-          builder: (context,snapshot){
+          stream:
+              FirebaseFirestore.instance.collection('Medicines').snapshots(),
+          builder: (context, snapshot) {
             var data = snapshot.data.docs[widget.index];
             return Container(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  TopImage(image: widget.image,),
                   Center(
-                    child: Text(data['name'],style: kHeadFontStyle.copyWith(fontSize: 35),),
-                  )
+                      child: TopImage(
+                    image: widget.image,
+                  )),
+                  Center(
+                    child: Text(
+                      data['name'],
+                      style: kHeadFontStyle.copyWith(fontSize: 35),
+                    ),
+                  ),
+                  RequirementRow()
                 ],
               ),
             );
@@ -34,6 +45,44 @@ class _ResourceInfoState extends State<ResourceInfo> {
         ),
       ),
     );
+  }
+
+  Row RequirementRow() {
+    return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "Your Requirement",
+                      style: TextStyle(
+                          fontFamily: "BebasNeue",
+                          fontWeight: FontWeight.w300,
+                          fontSize: 25),
+                    ),
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: (){
+                            setState(() {
+                              if(requirement>0){
+                                requirement--;
+                              }
+                            });
+                          },
+                          child: Icon(Icons.remove, color: Colors.red,size: 40,),
+                        ),
+                        Text(requirement.toString(),style: TextStyle(fontSize: 25),),
+                        GestureDetector(
+                          onTap: (){
+                            setState(() {
+                              requirement++;
+                            });
+                          },
+                          child: Icon(Icons.add ,color: Colors.green,size: 40,),
+                        ),
+                      ],
+                    )
+                  ],
+                );
   }
 }
 
@@ -48,12 +97,10 @@ class TopImage extends StatelessWidget {
         alignment: Alignment.bottomRight,
         children: [
           Container(
-            width: MediaQuery.of(context).size.width*0.9,
-            height: MediaQuery.of(context).size.height*0.15,
+            width: MediaQuery.of(context).size.width * 0.9,
+            height: MediaQuery.of(context).size.height * 0.15,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
-                color: Colors.blue
-            ),
+                borderRadius: BorderRadius.circular(25), color: Colors.blue),
             padding: EdgeInsets.zero,
           ),
           Hero(tag: 'img', child: image)
